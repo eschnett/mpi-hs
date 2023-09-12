3ac1ab1f8257bc43d4106baac34d3b0b99f7a475
Phillip Seeber <phillip.seeber@uni-jena.de>
2023-09-12 17:22:28 +0200

    cleanup of mpi detection and nix mechanism

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,16 +1,41 @@
 final: prev:
 
 let
-  haskellOverrides = hfinal: hprev: {
-    mpi-hs = hfinal.callPackage ./mpi-hs.nix { };
-  };
+  addMpiHsTestDeps = drv:
+    final.haskell.lib.compose.addTestToolDepends
+      [ final.openssh final.mpiCheckPhaseHook ]
+      drv;
 in
 {
   haskell = prev.haskell // {
-    packages = prev.haskell.packages // (builtins.mapAttrs
-      (key: val: val.override { overrides = haskellOverrides; })
-      prev.haskell.packages
-    );
+    packageOverrides = hfinal: hprev:
+      prev.haskell.packageOverrides hfinal hprev // {
+        mpi-hs = hfinal.mpi-hs_openmpi;
+
+        mpi-hs_openmpi =
+          addMpiHsTestDeps
+            (hfinal.callCabal2nixWithOptions "mpi-hs" ../. "-fopenmpi -f-mpich -f-mvapich" {
+              ompi = final.mpi;
+            });
+
+        mpi-hs_mpich =
+          addMpiHsTestDeps
+            (hfinal.callCabal2nixWithOptions "mpi-hs" ../. "-f-openmpi -fmpich -f-mvapich" {
+              mpich = final.mpich;
+            });
+
+        mpi-hs_mvapich =
+          addMpiHsTestDeps
+            (hfinal.callCabal2nixWithOptions "mpi-hs" ../. "-f-openmpi -f-mpich -fmvapich" {
+              mvapich2 = final.mvapich;
+            });
+
+        mpi-hs_mpi =
+          addMpiHsTestDeps
+            (hfinal.callCabal2nixWithOptions "mpi-hs" ../. "-f-openmpi -f-mpich -f-mvapich" {
+              openmpi = final.mpi;
+            });
+      };
   };
 }
 
